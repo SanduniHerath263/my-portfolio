51d5fd4493f59962824e1680963ef4545c090ec8
Sanduni Herath <sanduniherath263@gmail.com>
2025-08-20 02:33:38 +0530

    colorcontrols

--- portfolio-web/tailwind.config.js
+++ portfolio-web/tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        ink: '#0A0F1F',
+        sky: '#AEE4FF',
+        blueA: '#00B4DB',
+        blueB: '#0083B0',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
 }
